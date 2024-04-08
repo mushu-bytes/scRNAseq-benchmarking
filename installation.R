0d68287c1c582ef1b07c41d5f72a03fa65caa339
damon <dlin42@calpoly.edu>
2024-04-08 21:03:08 +0000

    installation script for seurat

---
+++ installation.R
@@ -0,0 +1,8 @@
+library(remotes)
+remotes::install_github("satijalab/seurat", "seurat5", quiet = TRUE)
+remotes::install_github("satijalab/seurat-data", "seurat5", quiet = TRUE)
+remotes::install_github("satijalab/azimuth", "seurat5", quiet = TRUE)
+remotes::install_github("satijalab/seurat-wrappers", "seurat5", quiet = TRUE)
+remotes::install_github("stuart-lab/signac", "seurat5", quiet = TRUE)
+remotes::install_github("bnprks/BPCells", quiet = TRUE)
+
